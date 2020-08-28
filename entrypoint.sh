e3b43b1d9e0375a47832a23ef26db7765def80db
rahul-deepsource <rahul@deepsource.io>
2020-08-28 16:33:54 +0530

    Add problem matchers; Also run Vulture

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,24 +9,28 @@
 # $5 - use-black
 # $6 - use-mypy
 # $7 - use-isort
-# $8 - extra-pylint-options
-# $9 - extra-pycodestyle-options
-# ${10} - extra-flake8-options
-# ${11} - extra-black-options
-# ${12} - extra-mypy-options
-# ${13} - extra-isort-options
+# $8 - use-vulture
+# $9 - extra-pylint-options
+# ${10} - extra-pycodestyle-options
+# ${11} - extra-flake8-options
+# ${12} - extra-black-options
+# ${13} - extra-mypy-options
+# ${14} - extra-isort-options
+# ${15} - extra-vulture-options
 
 if [ "$2" = true ] ; then
 
-    echo Running: pylint $8 $1
+    echo Running: pylint $9 $1
 
-    pylint $8 $1
+    pylint $9 $1
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
         echo "Pylint ok"
     else
         echo "Pylint error"
+        echo "::add-matcher::$(pwd)/.github/pylint-error.json"
+        echo "::add-matcher::$(pwd)/.github/pylint-warning.json"
         exit $exit_code
     fi
 
@@ -35,9 +39,9 @@ fi
 
 if [ "$3" = true ] ; then
 
-    echo Running: pycodestyle $9 $1
+    echo Running: pycodestyle ${10} $1
 
-    pycodestyle $9 $1
+    pycodestyle ${10} $1
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -51,15 +55,16 @@ fi
 
 if [ "$4" = true ] ; then
 
-    echo Running: flake8 ${10} $1
+    echo Running: flake8 ${11} $1
 
-    flake8 ${10} $1
+    flake8 ${11} $1
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
         echo "Flake8 ok"
     else
         echo "Flake8 error"
+        echo "::add-matcher::$(pwd)/.github/flake8.json"
         exit $exit_code
     fi
 
@@ -67,9 +72,9 @@ fi
 
 if [ "$5" = true ] ; then
 
-    echo Running: black --check ${11} $1
+    echo Running: black --check ${12} $1
 
-    black --check ${11} $1
+    black --check ${12} $1
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -83,15 +88,16 @@ fi
 
 if [ "$6" = true ] ; then
 
-    echo Running: mypy ${12} $1
+    echo Running: mypy ${13} $1
 
-    mypy ${12} $1
+    mypy ${13} $1
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
         echo "mypy ok"
     else
         echo "mypy error"
+        echo "::add-matcher::$(pwd)/.github/mypy.json"
         exit $exit_code
     fi
 
@@ -99,15 +105,32 @@ fi
 
 if [ "$7" = true ] ; then
 
-    echo Running: isort ${13} $1 -c --diff
+    echo Running: isort ${14} $1 -c --diff
 
-    isort ${13} $1 -c --diff
+    isort ${14} $1 -c --diff
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
         echo "isort ok"
     else
         echo "isort error"
+        exit $exit_code
+    fi
+
+fi
+
+if [ "$8" = true ] ; then
+
+    echo Running: vulture ${15} $1
+
+    vulture ${15} $1
+    exit_code=$?
+
+    if [ "$exit_code" = "0" ]; then
+        echo "vulture ok"
+    else
+        echo "vulture error"
+        echo "::add-matcher::$(pwd)/.github/vulture.json"
         exit $exit_code
     fi
 
